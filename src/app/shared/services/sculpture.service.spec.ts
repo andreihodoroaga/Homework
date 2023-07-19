0074a656a36c29b7828952dee0b32cd82c907d9f
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-07-19 17:08:55 +0300

    test: add unit tests for sculptures and orders (component + service)

--- src/app/shared/services/sculpture.service.spec.ts
+++ src/app/shared/services/sculpture.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SculptureService } from './sculpture.service';
+import { DataService } from './data.service';
+import { Sculpture } from '../models/sculpture';
+
+const testData: Sculpture[] = [
+  { id: '1', name: 'Sculpture 1', basePrice: 100, baseWeight: 200 },
+  { id: '2', name: 'Sculpture 2', basePrice: 200, baseWeight: 300 },
+];
+
+const mockDataService = {
+  getData: jasmine.createSpy('getData').and.returnValue({
+    pipe: () => ({
+      subscribe: (callback: (data: any) => void) => callback(testData),
+    }),
+  }),
+};
 
 describe('SculptureService', () => {
-  let service: SculptureService;
+  let sculptureService: SculptureService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SculptureService);
+    TestBed.configureTestingModule({
+      providers: [
+        SculptureService,
+        { provide: DataService, useValue: mockDataService },
+      ],
+    });
+    sculptureService = TestBed.inject(SculptureService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(sculptureService).toBeTruthy();
+  });
+
+  it('should get data correctly from the dataService', (done) => {
+    sculptureService.sculptureList$.subscribe((sculptures) => {
+      expect(mockDataService.getData).toHaveBeenCalledWith('get-sculptures');
+      expect(sculptures).toEqual(testData);
+      done();
+    });
   });
 });
