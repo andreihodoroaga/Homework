22bef7d5ca894dfb62ed837b17efd4cf2d86950f
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-07-21 13:31:53 +0300

    feat: add a dialog when the user wants to delete an order

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,12 +1,20 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+export interface DialogData {
+  title: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
 })
 export class ConfirmDialogComponent {
-  constructor(private dialogRef: MatDialogRef<ConfirmDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: {title: string, message: string}) {}
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
   onConfirm(): void {
     this.dialogRef.close(true);
--- src/app/shared/components/orders/orders.component.ts
+++ src/app/shared/components/orders/orders.component.ts
@@ -2,6 +2,8 @@ import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/
 import { OrderService } from '../../services/order.service';
 import { Order } from '../../models/order';
 import { Router } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-orders',
@@ -13,7 +15,25 @@ export class OrdersComponent {
   orders$ = this.orderService.orders$;
   deleteOrderMessage = '';
 
-  constructor(private orderService: OrderService, private router: Router, private cdRef: ChangeDetectorRef) { }
+  constructor(private orderService: OrderService, private router: Router, private cdRef: ChangeDetectorRef, private dialog: MatDialog) { }
+
+  async handleDeleteOrder(order: Order) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {
+      title: 'Confirm action',
+      message: 'Are you sure you want to delete this order?'
+    };
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result) {
+        await this.deleteOrder(order);
+      }
+    })
+  }
 
   async deleteOrder(order: Order) {
     if (this.deleteOrderMessage)
